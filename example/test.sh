26f9afbf4b44518f9314ba02f88573752e2bb783
司凯威(Kaisi Wei) <sikaiwei@genomics.cn>
2018-09-29 09:22:03 +0800

    update

--- example/test.sh
+++ example/test.sh
@@ -1,3 +1,4 @@
 perl ../plot.genome.featureCluster.pl --list list5 --prefix out --outdir . --conf main.conf
 perl ../plot.genome.featureCluster.pl --list list5 --prefix out2 --outdir . --conf main.2.conf
 perl ../plot.genome.featureCluster.pl --list list5 --prefix out3 --outdir . --conf main.3.conf
+perl ../plot.genome.featureCluster.pl --list list6 --prefix out6 --outdir . --conf main.6.conf
