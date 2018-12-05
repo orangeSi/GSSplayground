e02c0a6b610b2d77e9d649ec87e31c45358d4e0d
sikaiwei@genomics.cn <sikaiwei@software-install.cngb.sz.hpc>
2018-12-05 09:45:25 +0800

    add data

--- data/simulate.reads.sh
+++ data/simulate.reads.sh
@@ -3,10 +3,10 @@
 number_read=10000
 for i in $(ls s*.seq)
 do
-	#/zfssz5/BC_PS/sikaiwei/software/read_simulater/DWGSIM/dwgsim $i $i -N $number_read -1 150 -2 150 -c 0 -S 2 -H -z 1 -o 1 -r 0.02 -R 0.20 -z 2018
-	#sh bwa.sh ${i}.bwa.read1.fastq.gz,${i}.bwa.read2.fastq.gz $i . $i 3 && sh ${i}.bwa.sh
-	#/zfssz4/BC_COM_P0/F18FTSECKF1389/ASPjisD/software/pbsim/pbsim-1.0.3-Linux-amd64/Linux-amd64/bin/pbsim --data-type CLR --depth 20 --length-min 1000 --length-max 10000 --seed 0 --sample-fastq /zfssz4/BC_COM_P0/F18FTSECKF1389/ASPjisD/data/pb/chloroplast/sd_0001.fasta  $i --prefix $i.test
-	#cat ${i}.test*fastq >${i}.longreads.fq && rm $i.test*
+	/zfssz5/BC_PS/sikaiwei/software/read_simulater/DWGSIM/dwgsim $i $i -N $number_read -1 150 -2 150 -c 0 -S 2 -H -z 1 -o 1 -r 0.02 -R 0.20 -z 2018
+	sh bwa.sh ${i}.bwa.read1.fastq.gz,${i}.bwa.read2.fastq.gz $i . $i 3 && sh ${i}.bwa.sh
+	/zfssz4/BC_COM_P0/F18FTSECKF1389/ASPjisD/software/pbsim/pbsim-1.0.3-Linux-amd64/Linux-amd64/bin/pbsim --data-type CLR --depth 20 --length-min 1000 --length-max 10000 --seed 0 --sample-fastq /zfssz4/BC_COM_P0/F18FTSECKF1389/ASPjisD/data/pb/chloroplast/sd_0001.fasta  $i --prefix $i.test
+	cat ${i}.test*fastq >${i}.longreads.fq && rm $i.test*
 	/share/backup/sikaiwei/sikaiwei/assembly/sprai/sprai-0.9.9.19/fq2fa.pl  ${i}.longreads.fq >${i}.longreads.fa && gzip ${i}.longreads.fq
 	
 done
