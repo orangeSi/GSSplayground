accb887a618b5dac5bcbc4eee5cc160c23ce31ce
sikaiwei@genomics.cn <sikaiwei@software-install.cngb.sz.hpc>
2018-12-06 15:49:07 +0800

    update

---
+++ env.sh
@@ -0,0 +1,1 @@
+export PATH=/zfssz5/BC_PUB/Software/03.Soft_ALL/samtools-1.7:$PATH
--- example/work.sh
+++ example/work.sh
@@ -4,7 +4,7 @@ cp feature.crossing.link.conf9 feature.crossing.link.conf9.new
 
 perl ../prepare.data.pl --list list5 --prefix out9 --outdir . --conf main.10.conf
 
-cat s3.plot_depth.setting.conf feature.color.label.conf10.new >  feature.color.label.conf10.new
-cat  s3.plot_depth.crosslink feature.crossing.link.conf9.new >feature.crossing.link.conf9.new
+cat s[1234].{plot_depth,read_mapping}.setting.conf feature.color.label.conf10.old >  feature.color.label.conf10.new
+cat  feature.crossing.link.conf9 >feature.crossing.link.conf9.new
 
 perl ../plot.genome.featureCluster.pl --list list5.ytick --prefix out10 --outdir . --conf main.10.conf
