d86181bc3a882aaf47eb7a0856ae9da34747ffe7
sikaiwei@genomics.cn <sikaiwei@software-install.cngb.sz.hpc>
2018-12-21 15:47:38 +0800

    major update

--- example/out7.sh
+++ example/out7.sh
@@ -1,5 +1,5 @@
 set -vex
-perl ../prepare.data.pl --list list7 --prefix out7 --outdir . --conf main.conf
+perl ../prepare.data.pl --list list7 --prefix out7 --outdir . --conf main.7.conf
 cat s2.synteny.setting.conf s1.synteny.setting.conf s4.synteny.setting.conf s3.synteny.setting.conf >feature.color.label.conf7
 cat  s2.to.s4.synteny.crosslink  s1.to.s2.synteny.crosslink >feature.crossing.link.conf7
-perl ../plot.genome.featureCluster.pl --list list7.new --prefix out7 --outdir . --conf main.conf
+perl ../plot.genome.featureCluster.pl --list list7.new --prefix out7 --outdir . --conf main.7.conf
