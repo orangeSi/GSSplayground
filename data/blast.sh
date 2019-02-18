430b1c1d5dd90a00f42a242641a5d601f12e011b
sikaiwei@genomics.cn <sikaiwei@software-install.cngb.sz.hpc>
2019-02-18 16:28:56 +0800

    rm

--- data/blast.sh
+++
@@ -1,1 +0,0 @@
-/ifs4/BC_PUB/biosoft/pipeline/Package/myth/bin/blast/blastn.sh s4.seq s2.seq s2.mapto.s4.blast 3 100
--- data/paf.sh
+++
@@ -1,3 +0,0 @@
-minimap2 s2.seq s1.seq -cx asm5 |gzip >s1.mapto.s2.paf.gz
-minimap2 s3.seq s2.seq -cx asm5 |gzip >s2.mapto.s3.paf.gz
-minimap2 s4.seq s2.seq -cx asm5 |gzip >s2.mapto.s4.paf.gz
