95ee390fcfd85c5a0adbb122d5370a8c759befd9
sikaiwei@genomics.cn <sikaiwei@software-install.cngb.sz.hpc>
2019-02-19 12:21:22 +0800

    remove

--- example/map.sh
+++
@@ -1,4 +0,0 @@
-/ifshk7/BC_PS/sikaiwei/assembly/miniasm/minimap2/minimap2-2.11_x64-linux/minimap2 -x asm5  s1.seq s2.seq -c >s1.map.to.s2.paf
-less s1.map.to.s2.paf |awk -v prefix="fake" '{print $1"\tGenBank\tgene\t"$3"\t"$4"\t.\t.\t.\tID="$1"."prefix"."NR";\n"$6"\tGenBank\tgene\t"$8"\t"$9"\t.\t.\t.\tID="$6"."prefix"."NR";"}' >fake.gff
-cat s1.gff fake.gff >s1.add.fake.gff
-cat s2.gff fake.gff >s2.add.fake.gff
--- example/out12.sh
+++
@@ -1,9 +0,0 @@
-set -vex
-cp feature.color.label.conf12 feature.color.label.conf12.new
-cp feature.crossing.link.conf9 feature.crossing.link.conf12
-perl ../prepare.data.pl --list list5 --prefix out9 --outdir . --conf main.12.conf
-cat  s2.read_mapping.setting.conf s2.hist_scatter_line.setting.conf  s3.hist_scatter_line.setting.conf feature.color.label.conf12 >  feature.color.label.conf12.new
-
-cat s3.hist_scatter_line.crosslink feature.crossing.link.conf9.new >feature.crossing.link.conf12
-
-perl ../plot.genome.featureCluster.pl --list list5.ytick --prefix out12 --outdir . --conf main.12.conf
